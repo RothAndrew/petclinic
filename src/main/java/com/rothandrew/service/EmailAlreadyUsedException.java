77dd05a024ba88e281c320e5e8adb9b59a5cefcb
Andrew Roth <roth.andy@gmail.com>
2020-08-21 11:48:11 -0400

    Initial JHipster-Quarkus app generation

---
+++ src/main/java/com/rothandrew/service/EmailAlreadyUsedException.java
@@ -0,0 +1,8 @@
+package com.rothandrew.service;
+
+public class EmailAlreadyUsedException extends RuntimeException {
+
+    public EmailAlreadyUsedException() {
+        super("Email is already in use!");
+    }
+}
---
+++ src/main/java/com/rothandrew/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,8 @@
+package com.rothandrew.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
